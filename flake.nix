dbe6b0166b1b3bea39a33f92f952fbe2bb524ac1
Sam Lehman <github@samlehman.dev>
2023-12-11 01:01:49 -0500

    Use my development branch for `nixpkgs-gnome-mobile`
    ...for now.
    
    Also pass `specialArgs` to `nixosConfigurations`:
    - `inputs`
    - `user`

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       url = "github:nixos/mobile-nixos/development";
       flake = false;
     };
-    gnome-mobile.url = "github:lehmanator/nixpkgs-gnome-mobile";
+    gnome-mobile.url = "github:lehmanator/nixpkgs-gnome-mobile/develop";
     snowflake.url = "github:snowflakelinux/snowflake-modules";
     nix-data.url = "github:snowflakelinux/nix-data";
     nix-software-center.url = "github:vlinkz/nix-software-center";
@@ -27,23 +27,25 @@
 
       nixosConfigurations.fajita = nixosSystem {
         system = "aarch64-linux";
+        specialArgs = { inherit inputs; user = "sam"; };
         modules = [
-          { _module.args = { inherit inputs; }; }
+          { _module.args = { inherit inputs; user = "sam"; }; }
           (import "${inputs.mobile-nixos}/lib/configuration.nix" {
             device = "oneplus-fajita";
           })
           ./configuration.nix
-          ./snowflake.nix
-          inputs.snowflake.nixosModules.snowflake
-          inputs.nix-data.nixosModules."aarch64-linux".nix-data
+          #./snowflake.nix
+          #inputs.snowflake.nixosModules.snowflake
+          #inputs.nix-data.nixosModules."aarch64-linux".nix-data
           inputs.gnome-mobile.nixosModules.gnome-mobile
         ];
       };
 
       nixosConfigurations.uefi-x86_64 = nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; user = "sam"; };
         modules = [
-          { _module.args = { inherit inputs; }; }
+          { _module.args = { inherit inputs; user = "sam"; }; }
           (import "${inputs.mobile-nixos}/lib/configuration.nix" {
             device = "uefi-x86_64";
           })
@@ -54,8 +56,9 @@
 
       nixosConfigurations.fajita_minimal = nixosSystem {
         system = "aarch64-linux";
+        specialArgs = { inherit inputs; user = "sam"; };
         modules = [
-          { _module.args = { inherit inputs; }; }
+          { _module.args = { inherit inputs; user = "sam"; }; }
           (import "${inputs.mobile-nixos}/lib/configuration.nix" {
             device = "oneplus-fajita";
           })
@@ -65,5 +68,19 @@
       fajita-fastboot-images = inputs.self.nixosConfigurations.fajita.config.mobile.outputs.android.android-fastboot-images;
       fajita-minimal-image = inputs.self.nixosConfigurations.fajita_minimal.config.mobile.outputs.android.android-fastboot-images;
       uefi-x86_64-image = inputs.self.nixosConfigurations.uefi-x86_64.config.mobile.outputs.default;
-    };
+    } // inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ (import inputs.gnome-mobile.overlays.default) ]; };
+      in
+      {
+        packages = {
+          fajita-fastboot-images = inputs.self.nixosConfigurations.fajita.config.mobile.outputs.android.android-fastboot-images;
+          fajita-minimal-image = inputs.self.nixosConfigurations.fajita_minimal.config.mobile.outputs.android.android-fastboot-images;
+          uefi-x86_64-image = inputs.self.nixosConfigurations.uefi-x86_64.config.mobile.outputs.default;
+          #gnome-shell-mobile = (pkgs.callPackage pkgs.gnome-shell-mobile);
+          #gnome-shell-mobile-devel = pkgs.gnome-shell-mobile-devel;
+          #mutter-mobile = pkgs.mutter-mobile;
+          #mutter-mobile-devel = pkgs.mutter-mobile-devel;
+        };
+      });
 }
